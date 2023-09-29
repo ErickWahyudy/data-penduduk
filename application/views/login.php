11bdb15a8d47ecc881970317a6ea62670425aaf7
ErickWahyudy <erickwahyudy@gmail.com>
2023-09-29 14:44:02 +0700

    update view login

--- application/views/login.php
+++ application/views/login.php
@@ -9,106 +9,71 @@
   <meta name="description" content="Pendataan penduduk dan keluarga guna mempermudah dalam proses pelayanan administrasi dengan menggunakan teknologi informasi">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="<?= base_url('themes/admin/') ?>/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url('themes/admin/') ?>/bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="<?= base_url('themes/admin/') ?>/bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url('themes/admin/') ?>/dist/css/AdminLTE.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="<?= base_url('themes/admin/') ?>/plugins/iCheck/square/blue.css">
+  <!-- Bootstrap 4.5.2 -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+ 
+  <!-- Icon Font Stylesheet -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
   <!-- Favicon -->
   <link href="<?= base_url('themes/kassandra-wifi') ?>/img/favicon.ico" rel="icon">
-  <!-- manifest json -->
-  <link rel="manifest" href="<?= base_url('static/manifest.json') ?>">
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <!-- sweetalert -->
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css'>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
 </head>
-<!-- <div class="content">
-  <div class="col-md-12">
- <img src="<?= base_url('themes/logo.png') ?>" class="img-responsive">
-</div>
-</div> -->
-
-<!-- sweetalert -->
-    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css'>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
-
-<body class="hold-transition" background="<?= base_url('themes/admin') ?>/dist/img/bgmember.jpg" style="background-size: cover; background-attachment: fixed;">
-<div class="login-box">
-   
-<?= $this->session->flashdata('pesan') ?>
-		<!-- /.login-logo -->
-		<div class="login-box-body">
-			<center>
-				<img src="<?= base_url('themes/admin') ?>/dist/img/penduduk-01.png" width="100%" />
-				<h4>
-					<b>
-						Web Aplikasi Data Penduduk
-					</b>
-				</h4>
-				
-			</center>
-    <form action="" method="post">
-      <div class="form-group has-feedback">
-        <input type="text" class="form-control" name="email" placeholder="Nama / Email / No KK / No HP" required="">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" name="password" placeholder="Password" required="">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <!-- /.col -->
-        <div class="box-footer">
-						<button type="submit" class="btn btn-primary btn-block" name="login" title="Masuk Sistem">
-							<b>LOGIN</b>
-						</button>
-          <div class="checkbox icheck">
-            <label>
-              lupa password? <a href="<?= base_url('reset_password') ?>">klik disini</a>
-            </label> <br>
-            <label>
-              Cari data warga tanpa login? <a href="<?= base_url('cari_penduduk') ?>">klik disini</a>
-            </label>
-          </div>
-        		<center>
-							<strong>Copyright &copy; <?php echo date('Y'); ?>
-							<a href="https://bit.ly/kassandrahdproduction" target="blank">Kassandra Production</a>.</strong> All rights reserved.
-						</center>
-					</div>
-      </div>
-    </form>
-   <!--  <a href="#">I forgot my password</a><br> -->
-  </div>
-  <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
-
-<!-- jQuery 3 -->
-<script src="<?= base_url('themes/admin/') ?>/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="<?= base_url('themes/admin/') ?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="<?= base_url('themes/admin/') ?>/plugins/iCheck/icheck.min.js"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' /* optional */
-    });
-  });
-</script>
+<body style="background: url('<?= base_url('themes/admin/dist/img/windows11.jpg') ?>') no-repeat center center fixed; background-size: cover;">
+<div class="container mt-5 mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header text-center">
+                    <img src="<?= base_url('themes/admin') ?>/dist/img/penduduk-01.png" width="100%" />
+                        <h5 class="card-title">
+                          <b>
+                          Web Aplikasi Data Penduduk
+                          </b>
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        <?= $this->session->flashdata('pesan') ?>
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <div class="input-group">
+                                <input type="text" class="form-control" name="email" placeholder="Nama / Email / No KK / No HP" required="">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    </div>
+                                  </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required="">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                                    </div>
+                                  </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block" name="login">Login</button>
+                            <div class="form-group mt-2">
+                                <a href="<?= base_url('reset_password') ?>">Lupa password?</a> <br>
+                                Cari data penduduk? <a href="<?= base_url('cari_penduduk') ?>">klik disini</a>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer text-center">
+                      <p>
+                        <strong>Copyright &copy; <?php echo date('Y'); ?>
+                        <a href="https://bit.ly/kassandrahdproduction" target="_blank">Kassandra Production</a>.</strong> All rights reserved.
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Tautan ke file JavaScript Bootstrap 4 (jika diperlukan) -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
