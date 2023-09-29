9a88436c37818fa6aa4e156bb00337f0ba77b1c5
ErickWahyudy <erickwahyudy@gmail.com>
2023-09-29 20:02:35 +0700

    update view login

--- application/views/login.php
+++ application/views/login.php
@@ -41,7 +41,7 @@
                         <form action="" method="post">
                             <div class="form-group">
                                 <div class="input-group">
-                                <input type="text" class="form-control" name="email" placeholder="Nama / Email / No KK / No HP" required="">
+                                <input type="text" class="form-control" name="email" placeholder="Nama / Email / No HP" required="">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="fas fa-user"></i></span>
                                     </div>
